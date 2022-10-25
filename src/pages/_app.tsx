78f8b768ebc4c77eb10cb89d4fb83dc42e43aae7
Cloé <61829801+cgslbs@users.noreply.github.com>
2022-10-25 15:27:07 +0200

    feat: :package:  : Added dependencies
    - Axios
    - Mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { MantineTheme } from "@mantine/core";
+import { DEFAULT_THEME } from "@mantine/styles";
+
+export const theme : MantineTheme = {
+    ...DEFAULT_THEME,
+    primaryColor:"blue",
+    colorScheme:"light"
+}
+
