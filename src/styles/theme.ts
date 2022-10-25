a1a72aa98ea0a80bb6accb0a4cf7542b6dd0e2b1
Cloé <61829801+cgslbs@users.noreply.github.com>
2022-10-25 16:43:12 +0200

    style: :star: add theme
    - using Mantine UI

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,11 @@
 import { MantineTheme } from "@mantine/core";
 import { DEFAULT_THEME } from "@mantine/styles";
 
-export const theme : MantineTheme = {
-    ...DEFAULT_THEME,
-    primaryColor:"blue",
-    colorScheme:"light"
-}
+export const theme: MantineTheme = {
+  ...DEFAULT_THEME,
+  primaryColor: "blue",
+  colorScheme: "light",
+};
+
+
 
